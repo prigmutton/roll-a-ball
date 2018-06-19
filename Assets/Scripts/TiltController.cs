47faf362a44439412f336fa5f103ce0a63135a82
prigmutton <xaos@mindspring.com>
2018-06-19 10:50:49 -0400

    Initial state (roll a ball including pickups and surface tilt; no score tracking

---
+++ Assets/Scripts/TiltController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltController : MonoBehaviour {
+    public float maxTilt;
+
+    private float xRot;
+    private float zRot;
+
+	// Use this for initialization
+	void Start () {
+        xRot = 0.0f;
+        zRot = 0.0f;
+    }
+
+    void LateUpdate()
+    {
+        float moveHorizontal =  Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        if (xRot > maxTilt)
+        {
+            moveVertical = maxTilt - xRot;
+        }
+        else if (xRot < (-1 * maxTilt))
+        {
+            moveVertical = (-1 * maxTilt) - xRot;
+        }
+        xRot += moveVertical;
+
+        if (zRot > maxTilt)
+        {
+            moveHorizontal = maxTilt - zRot;
+        }
+        else if (zRot < (-1 * maxTilt))
+        {
+            moveHorizontal = (-1 * maxTilt) - zRot;
+        }
+        zRot += moveHorizontal;
+
+        Vector3 movement = new Vector3(moveVertical, 0.0f, -moveHorizontal);
+
+        transform.Rotate(movement * 0.25f);
+    }
+}
